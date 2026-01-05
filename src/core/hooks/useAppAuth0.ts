67c3ee29f4c7821dc0a73bd609356d445bcb4699
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2026-01-05 16:37:39 -0600

    Create useAppAuth0

---
+++ src/core/hooks/useAppAuth0.ts
@@ -0,0 +1,33 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+export const useAppAuth0 = () => {
+  const {
+    isLoading,
+    isAuthenticated,
+    error,
+    loginWithRedirect: login,
+    logout: auth0Logout,
+    user,
+    getAccessTokenSilently,
+  } = useAuth0();
+
+  const getToken = async () => {
+    return await getAccessTokenSilently({
+      authorizationParams: {
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
+      },
+    });
+  };
+
+  const logout = () => auth0Logout({ logoutParams: { returnTo: window.location.origin } });
+
+  return {
+    isLoading,
+    isAuthenticated,
+    error,
+    login,
+    user,
+    getToken,
+    logout,
+  };
+};
