5c51557eed132521bfd9f17d0915b49f2eb12c1f
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2026-01-05 16:12:21 -0600

    Add Auth0Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { AppAuth0Provider } from "./AppAuth0Provider";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AppAuth0Provider>
+      <App />
+    </AppAuth0Provider>
+  </StrictMode>
+);
