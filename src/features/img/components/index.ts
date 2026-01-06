c831f78d2b6af95fa1e3f9e80bc8989c3f1bf2e0
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2026-01-06 14:14:14 -0600

    Create barrel file

---
+++ src/features/img/components/index.ts
@@ -0,0 +1,5 @@
+export { ChatSection } from "./ChatSection";
+export { InputSection } from "./InputSection";
+export { PromptFilePreview } from "./PromptFilePreview";
+export { UserMessage } from "./UserMessage";
+export { AssistantMessage } from "./AssistantMessage";
